d47f4c48d439432e8d729f3274deb85ac2b55ebb
nittesh <o.nittesh@gmail.com>
2021-10-26 07:44:20 +0530

    added another file

--- shellsc.sh
+++ shellsc.sh
@@ -3,3 +3,5 @@ echo nittesh
 read -p enter the age
 please enter your name before the age 
 we use the git commands for the files changes nad moification
+git is like pipe connection to send the code to another remot server
+
