c18dca86cb1edb5ef1ce78ef1e2ec76b7d4e7775
nittesh <o.nittesh@gmail.com>
2021-10-26 09:00:44 +0530

    2 files are edited

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,4 @@
 !#/bin/bash
 hi 
 this practicing files
+to commit this file we use a command that command is git commit -m "write here why we are commiting and what are the changes"
--- shellsc.sh
+++ shellsc.sh
@@ -5,3 +5,5 @@ please enter your name before the age
 we use the git commands for the files changes nad moification
 git is like pipe connection to send the code to another remot server
 
+to unstage the file we use a commad
+
